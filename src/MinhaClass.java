20f42e3735236cb216d7246e5e2e25c7e8231e21
Eduardo <eduardo25082002@gmail.com>
2024-09-16 16:22:48 -0300

    Segundo Commit

--- src/MinhaClass.java
+++ src/MinhaClass.java
@@ -2,8 +2,34 @@ public class MinhaClass {
     // Corpo da classe
     public static void main (String [] args) {
         // Corpo do método
-        System.out.print("Bom Dia, Meus Amigos\n"); 
+        System.out.print("Bom Dia, Meus Amigos\n"); // Comando para imprimir uma mensagem 
+     
+        final String BR = "Brasil"; 
+        // Declarei a Var BR e atribui o valor de Brasil a ela.
+        // Através da regra  'final' a var não poderá sofrer alteração durante meu programa.
 
+        final double PI = 3.14;
+        final int ESTADOS_BRASILEIROS = 27;
+        final int ANO_2000 = 20;
+
+        
     }
     
 }
+
+/* DECLARAÇÕES INVÁLIDAS
+ *  int numero&um = 1; Os unicos simbolos permitidos são _ e $
+ *  int 1numero = 1; Uma VAR não pode começar com núemro
+ *  int numero um = 1; uma VAR não pode ter espaços
+ *  int long = 1; LONG faz parte das palavras reservadas da ling.
+ */
+
+
+ /* DECLARAÇÕES VÁLIDAS (Corrigidas)
+  * int numero$Um = 1;
+    int numero1 = 1;
+    int numeroum = 1;
+    int longo = 1;
+  */
+
+
