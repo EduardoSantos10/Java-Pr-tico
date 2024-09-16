4e0d2a3709f5f841acfcfbfcdf1191baaf1105b2
Eduardo <eduardo25082002@gmail.com>
2024-09-16 13:32:56 -0300

    Pirmeiro Commit

---
+++ src/MinhaClass.java
@@ -0,0 +1,9 @@
+public class MinhaClass {
+    // Corpo da classe
+    public static void main (String [] args) {
+        // Corpo do método
+        System.out.print("Bom Dia, Meus Amigos\n"); 
+
+    }
+    
+}
